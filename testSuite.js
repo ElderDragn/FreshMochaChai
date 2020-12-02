dfaa3a1e7180dc4c5765210d4aca0720400e0fc1
Jacob Eldred <jeldred95@gmail.com>
2020-12-02 00:39:41 -0800

    Added testRunner & testSuite files

---
+++ testSuite.js
@@ -0,0 +1,53 @@
+var should = chai.should();
+var assert = chai.assert;
+var expect = chai.expect;
+describe('Suite Title', function() {
+
+  beforeEach(function() {
+    trueVar = true;
+    obj1 = {a: 2};
+    obj2 = {a: 2};
+  });
+
+  it('Passing test', function() {
+    (trueVar).should.equal(true);
+  });
+
+  it('Failing test', function() {
+    (trueVar).should.equal(false);
+  });
+
+  it('Deep equality test', function() {
+    (obj1).should.eql(obj2);
+  });
+
+  xit('More tests', function() {
+    (trueVar).should.equal(true);
+  });
+
+  xit('More tests', function() {
+    (trueVar).should.equal(true);
+  });
+
+  describe('Nested Suite', function() {
+
+    xit('More tests', function() {
+      (trueVar).should.equal(true);
+    });
+
+    xit('More tests', function() {
+      (trueVar).should.equal(true);
+    });
+
+    xit('More tests', function() {
+      (trueVar).should.equal(true);
+    });
+
+  });
+
+
+
+});
+
+
+
